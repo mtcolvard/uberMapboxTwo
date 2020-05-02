a09ea982a59e393dfaaabd405a4e5ad7dd18e1ab
mtcolvard <matthew.colvard@gmail.com>
2020-05-02 21:18:48 +0200

    stable, moving to foxit to continue with its python backend

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactMapGl, {NavigationControl, GeolocateControl} from 'react-map-gl'
+import ReactMapGl, {BaseControl, NavigationControl, GeolocateControl, LinearInterpolator, FlyToInterpolator, HTMLOverlay} from 'react-map-gl'
 import axios from 'axios'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
@@ -10,34 +10,92 @@ const geolocateStyle = {
   margin: 10
 }
 
+const navigationControlStyle = {
+  position: 'absolute',
+  right: 0
+}
+
+const fieldStyle = {
+  width: '100vw',
+  height: '5vh',
+  position: 'absolute',
+  bottom: 0,
+  left: 0
+
+}
+
+
+const lngLat = [-0.084254, 51.518961]
+
 class Map extends React.Component {
   constructor() {
     super()
     this.state = {
-      viewport: {longitude: -0.084254, latitude: 51.518961, zoom: 12}
+      viewport: {longitude: lngLat[0], latitude: lngLat[1], zoom: 12},
+      formData: {}
+
     }
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleMouseDown( {lngLat} ) {
+    this.setState({viewport: {
+      longitude: lngLat[0],
+      latitude: lngLat[1],
+      zoom: 12,
+      transitionDuration: 1000,
+      transitionInterpolator: new FlyToInterpolator({
+        curve: 2.4})
+    }})
+  }
+
+  handleChange(e) {
+      const formData = { ...this.state.formData, [e.target.name]: e.target.value }
+      this.setState({ formData, error: '' })
+    }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log('return successful')
+
+  }
+
 
   render () {
     const {viewport} = this.state
     return (
-      <ReactMapGl {...viewport}
-        mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        height='100vh'
-        width='100vw'
-        onViewportChange={viewport => this.setState({viewport})}>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="input"
+            type="text"
+            placeholder="Add destination to plan route"
+            onChange={this.handleChange}
+          />
+        </form>
         <div>
-        <GeolocateControl
-          style={geolocateStyle}
-          positionOptions={{enableHighAccuracy: true}}
-          trackUserLocation={true}
-        />
+          <ReactMapGl {...viewport}
+            mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            height='100vh'
+            width='100vw'
+            onViewportChange={viewport => this.setState({viewport})}
+            onClick={this.handleMouseDown}>
+            <div>
+              <GeolocateControl
+                style={geolocateStyle}
+                positionOptions={{enableHighAccuracy: true}}
+                trackUserLocation={true}
+                captureClick={false}
+              />
+            </div>
+            <div style={navigationControlStyle}>
+              <NavigationControl/>
+            </div>
+          </ReactMapGl>
         </div>
-        <div style={{position: 'absolute', right: 0}}>
-          <NavigationControl />
-        </div>
-      </ReactMapGl>
+      </div>
     )
   }
 }
